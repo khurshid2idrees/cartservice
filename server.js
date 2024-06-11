1cf34f649c1530a3416b51064186be82b29834f8
khurshid idrees <umarkhurshid3@gmail.com>
2024-06-12 04:27:31 +0530

    update in server jss and package json

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const orderModel = require('./Models/orderModel');
+const cors = require("cors");
+const orderModel = require("./Models/orderModel");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // connect the database here
 
@@ -14,8 +21,16 @@ async function connectDatabase() {
   console.log("database connected");
 }
 
+// create new order
 
-app.
+app.post("/orders", (req, res) => {
+  const orderData = req.body;
+
+  orderModel
+    .create(orderData)
+    .then((result) => res.json("success"))
+    .catch((err) => console.log(err));
+});
 
 app.listen("5000", () => {
   console.log("the server started at port 5000");
